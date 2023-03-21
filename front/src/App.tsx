fd595694d72450643c38de92512c16ae080c92fa
Jeniffer Cristina <88725100+Jennyads@users.noreply.github.com>
2023-03-21 10:52:43 -0300

    Add files via upload

---
+++ front/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>oi {2+2}</div>
+      <div>Até segunda</div>
+    </>
+  );
+}
+
+export default App;
